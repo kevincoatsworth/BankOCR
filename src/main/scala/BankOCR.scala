f3bbc575bde349b71a1be3d8cbf78837b8e49745
Michael Bailey <michael.bailey3@digital.hmrc.gov.uk>
2018-03-27 14:56:25 +0100

    - Added reverseListIndex
    -Added mod to check acc number is mod of 11

--- src/main/scala/BankOCR.scala
+++ src/main/scala/BankOCR.scala
@@ -1,54 +1,60 @@
 object BankOCR extends App {
 
   def numberChecker(string: String): String = {
-
     val numMap = Map (
       " _ " +
       "| |" +
-      "|_|" -> "0",
+      "|_|" -> 0,
       "   " +
       "  |" +
-      "  |" -> "1",
+      "  |" -> 1,
       " _ " +
       " _|" +
-      "|_ " -> "2",
+      "|_ " -> 2,
       " _ " +
       " _|" +
-      " _|" -> "3",
+      " _|" -> 3,
       "   " +
       "|_|" +
-      "  |" -> "4",
+      "  |" -> 4,
       " _ " +
       "|_ " +
-      " _|" -> "5",
+      " _|" -> 5,
       " _ " +
       "|_ " +
-      "|_|" -> "6",
+      "|_|" -> 6,
       " _ " +
       "  |" +
-      "  |" -> "7",
+      "  |" -> 7,
       " _ " +
       "|_|" +
-      "|_|" -> "8",
+      "|_|" -> 8,
       " _ " +
       "|_|" +
-      " _|" -> "9"
+      " _|" -> 9
     )
-
-    println(string.length)
-
-   val numbers = string.length match {
+    val numbers = string.length match {
 
       case x if x % 9 == 0 => string.length/9
       case _ => throw new IllegalArgumentException
     }
 
-    val reg = ".{3}".r
-    val split = reg.findAllIn(string).toList
+
+    val regex = ".{3}".r
+    val split = regex.findAllIn(string).toList
     val stringList = split.grouped(numbers).toList.transpose
     val strings = stringList.map(s => s.mkString)
-    val reverseNumber = strings.map(s => numMap(s)).reverse
+
     val check = strings.map(s => numMap(s)).mkString
+
+    val reverseNumber = strings.map(s => numMap(s)).reverse
+
+
+    val reverseListWithIndex = reverseNumber.zipWithIndex.map(x => x._1*(x._2 + 1)).sum
+    val mod = reverseListWithIndex % 11 == 0
+
+
+    println(mod)
 
     check
   }
