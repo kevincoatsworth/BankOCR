b13baac9b40e6d6e92185409ac30292c8a69b824
Michael Bailey <michael.bailey3@digital.hmrc.gov.uk>
2018-03-27 15:56:44 +0100

    Refactor code into functions.

--- src/main/scala/BankOCR.scala
+++ src/main/scala/BankOCR.scala
@@ -1,66 +1,92 @@
 object BankOCR extends App {
 
-  def numberChecker(string: String): String = {
-    val numMap = Map (
-      " _ " +
-      "| |" +
-      "|_|" -> 0,
-      "   " +
-      "  |" +
-      "  |" -> 1,
-      " _ " +
-      " _|" +
-      "|_ " -> 2,
-      " _ " +
-      " _|" +
-      " _|" -> 3,
-      "   " +
-      "|_|" +
-      "  |" -> 4,
-      " _ " +
-      "|_ " +
-      " _|" -> 5,
-      " _ " +
-      "|_ " +
-      "|_|" -> 6,
-      " _ " +
-      "  |" +
-      "  |" -> 7,
-      " _ " +
-      "|_|" +
-      "|_|" -> 8,
-      " _ " +
-      "|_|" +
-      " _|" -> 9
-    )
-    val numbers = string.length match {
+  val numMap = Map(
+    " _ " +
+    "| |" +
+    "|_|" -> 0,
+    "   " +
+    "  |" +
+    "  |" -> 1,
+    " _ " +
+    " _|" +
+    "|_ " -> 2,
+    " _ " +
+    " _|" +
+    " _|" -> 3,
+    "   " +
+    "|_|" +
+    "  |" -> 4,
+    " _ " +
+    "|_ " +
+    " _|" -> 5,
+    " _ " +
+    "|_ " +
+    "|_|" -> 6,
+    " _ " +
+    "  |" +
+    "  |" -> 7,
+    " _ " +
+    "|_|" +
+    "|_|" -> 8,
+    " _ " +
+    "|_|" +
+    " _|" -> 9
+  )
+  def numberChecker(string: String): List[Int] = {
 
-      case x if x % 9 == 0 => string.length/9
+    val numbers = string
+                  .length match {
+      case x if x % 9 == 0 => string
+                              .length / 9
       case _ => throw new IllegalArgumentException
     }
 
+    val regex = ".{3}"
+      .r
+    val split = regex
+                .findAllIn(string)
+                .toList
+    val stringList = split
+                     .grouped(numbers)
+                     .toList
+                     .transpose
+    val strings = stringList
+                  .map(s => s
+                            .mkString)
+    val check = strings
+                .map(s => numMap(s))
 
-    val regex = ".{3}".r
-    val split = regex.findAllIn(string).toList
-    val stringList = split.grouped(numbers).toList.transpose
-    val strings = stringList.map(s => s.mkString)
 
-    val check = strings.map(s => numMap(s)).mkString
+    println(check)
+    check
 
-    val reverseNumber = strings.map(s => numMap(s)).reverse
+  }
+
+  def checkSum(resultOfChecker : List[Int]): Boolean = {
+
+    val reverseNumber = resultOfChecker.reverse
 
 
     val reverseListWithIndex = reverseNumber.zipWithIndex.map(x => x._1*(x._2 + 1)).sum
-    val mod = reverseListWithIndex % 11 == 0
+    val mod =reverseListWithIndex % 11 == 0
 
 
     println(mod)
 
-    check
+    mod
   }
-  numberChecker(" _     _  _  _  _  _  _  _ " +
-                " _||_||_ |_||_| _||_||_ |_ " +
-                " _|  | _||_||_||_ |_||_| _|")
+
+  def start(){
+
+    val numCheck = numberChecker(" _     _  _  _  _  _  _  _ " +
+                                 " _||_||_ |_||_| _||_||_ |_ " +
+                                 " _|  | _||_||_||_ |_||_| _|")
+
+    checkSum(numCheck)
+
+  }
+  start()
+
 }
 
 
