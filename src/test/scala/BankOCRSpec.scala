32a744d2a7c244e6fba4c5739de51a88f3014b53
Kevin Coatsworth <kevin.coatsworth@digital.hmrc.gov.uk>
2018-03-28 10:55:38 +0100

    Refactor tests for refactored functions.

--- src/test/scala/BankOCRSpec.scala
+++ src/test/scala/BankOCRSpec.scala
@@ -7,41 +7,41 @@ class BankOCRSpec extends WordSpec with MustMatchers {
     "return 4 when provided with '\"   \" +\n\"|_|\" +\n\"  |\"' " in {
       BankOCR.numberChecker("   " +
                             "|_|" +
-                            "  |" ) mustEqual "4"
+                            "  |" ) mustEqual List("4")
     }
 
     "return 9 when provided with the appropriate input " in {
       BankOCR.numberChecker(" _ " +
                             "|_|" +
-                            " _|" ) mustEqual "9"
+                            " _|" ) mustEqual List("9")
     }
 
     "return 8 when provided with the appropriate input " in {
       BankOCR.numberChecker(" _ " +
                             "|_|" +
-                            "|_|" ) mustEqual "8"
+                            "|_|" ) mustEqual List("8")
     }
 
     "return 48 when provided with the appropriate input " in {
       BankOCR.numberChecker("    _ " +
                             "|_||_|" +
-                            "  ||_|" ) mustEqual "48"
+                            "  ||_|" ) mustEqual List("4","8")
     }
 
     "return 12 when provided with the appropriate input " in {
       BankOCR.numberChecker("    _ " +
                             "  | _|" +
-                            "  ||_ " ) mustEqual "12"
+                            "  ||_ " ) mustEqual List("1","2")
     }
 
     "return 123456789 when provided with the appropriate input" in {
       BankOCR.numberChecker("    _  _     _  _  _  _  _ " +
                             "  | _| _||_||_ |_   ||_||_|" +
-                            "  ||_  _|  | _||_|  ||_| _|" ) mustEqual "123456789"
+                            "  ||_  _|  | _||_|  ||_| _|" ) mustEqual List("1","2","3","4","5","6","7","8","9")
     }
 
     "return true if 9 digit account number is modulos of 11" in {
-
+      BankOCR.checkSum(List("3","4","5","8","8","2","8","6","5")) mustEqual true
 
     }
 
